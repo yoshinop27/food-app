6898682a3f6958a9f5aeeacf5ce3cf9afd07ccfb
Preston Yoshino <prestonyoshino@gmail.com>
2026-03-07 01:47:56 -0600

    added header states

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,23 +1,41 @@
 import { Box, Typography, Slider, Autocomplete, TextField } from "@mui/material";
 import { foodTypeOptions } from "../util/food-types";
+import { useState } from "react";
 
 const Header = () => {
+
+  const [price, setPrice] = useState(50);
+  const [distance, setDistance] = useState(20);
+  const [type, setType] = useState();
+
+  const handleChangePrice = (event, newValue) => {
+    setPrice(newValue);
+  }
+
+  const handleChangeDistance = (event, newValue) => {
+    setDistance(newValue);
+  }
+
+  const handleChangeType = (event, newValue) => {
+    setType(newValue);
+  }
+
   return (
     <Box sx={{ width: "80%", display: "flex", alignItems: "center", gap: 5, px: 2, justifyContent: "center" }} >
-        <Typography variant="h4" component="h1">Header</Typography>
+        <Typography variant="h4" component="h1">FoodFinder</Typography>
 
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-            <Typography variant="h6" sx={{ width: "100%", textAlign: "left" }}>Distance</Typography>
-            <Slider defaultValue={20} sx={{ width: 240, maxWidth: "45vw" }} />
+            <Typography variant="h6" sx={{ width: "100%", textAlign: "left" }}>Distance (mi): {distance}</Typography>
+            <Slider defaultValue={distance} onChange={handleChangeDistance} sx={{ width: 240, maxWidth: "45vw" }} />
         </Box>
     
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-            <Typography variant="h6" sx={{ width: "100%", textAlign: "left" }}>Price</Typography>
-            <Slider defaultValue={20} sx={{ width: 240, maxWidth: "45vw" }} />
+            <Typography variant="h6" sx={{ width: "100%", textAlign: "left" }}>Price ($): {price} </Typography>
+            <Slider defaultValue={price} onChange={handleChangePrice} sx={{ width: 240, maxWidth: "45vw" }} />
         </Box>
 
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-            <Autocomplete options={foodTypeOptions} sx={{ width: 240, maxWidth: "45vw" }} renderInput={(params) => <TextField {...params} label="Food Type" />} />
+            <Autocomplete value={type} onChange={handleChangeType} options={foodTypeOptions} sx={{ width: 240, maxWidth: "45vw" }} renderInput={(params) => <TextField {...params} label="Food Type" />} />
         </Box>
     </Box>
   )
