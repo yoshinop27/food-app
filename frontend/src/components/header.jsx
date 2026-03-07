aea2a8d9d6da89adc78114965a02001330fceb18
Preston Yoshino <prestonyoshino@gmail.com>
2026-03-07 00:48:26 -0600

    working on adding state to header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,3 +1,26 @@
-import React from 'react'
+import { Box, Typography, Slider, Autocomplete, TextField } from "@mui/material";
+import { foodTypeOptions } from "../util/food-types";
 
-const header = 1
+const Header = () => {
+  return (
+    <Box sx={{ width: "80%", display: "flex", alignItems: "center", gap: 5, px: 2, justifyContent: "center" }} >
+        <Typography variant="h4" component="h1">Header</Typography>
+
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+            <Typography variant="h6" sx={{ width: "100%", textAlign: "left" }}>Distance</Typography>
+            <Slider defaultValue={20} sx={{ width: 240, maxWidth: "45vw" }} />
+        </Box>
+    
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+            <Typography variant="h6" sx={{ width: "100%", textAlign: "left" }}>Price</Typography>
+            <Slider defaultValue={20} sx={{ width: 240, maxWidth: "45vw" }} />
+        </Box>
+
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+            <Autocomplete options={foodTypeOptions} sx={{ width: 240, maxWidth: "45vw" }} renderInput={(params) => <TextField {...params} label="Food Type" />} />
+        </Box>
+    </Box>
+  )
+};
+
+export default Header;
---
+++ frontend/src/util/food-types.js
@@ -0,0 +1,18 @@
+export const foodTypes = {
+  american: "American",
+  chinese: "Chinese",
+  french: "French",
+  greek: "Greek",
+  indian: "Indian",
+  italian: "Italian",
+  japanese: "Japanese",
+  korean: "Korean",
+  mediterranean: "Mediterranean",
+  mexican: "Mexican",
+  middleEastern: "Middle Eastern",
+  spanish: "Spanish",
+  thai: "Thai",
+  vietnamese: "Vietnamese",
+};
+
+export const foodTypeOptions = Object.values(foodTypes);
