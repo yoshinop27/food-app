a588880798ff3b64372c5a2784d3d1e88793a671
Preston Yoshino <prestonyoshino@gmail.com>
2026-03-07 21:35:49 -0600

    added marker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { Box } from '@mui/material'
 import Header from './components/header'
+import Map from './components/map'
 
 function App() {
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Header />
+    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', bottompadding: '5rem', gap: '2rem', alignItems: 'center' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Header />
+      </Box>
+      <Box sx={{ width: '90%', height: '70vh', justifyContent: 'center' }}>
+        <Map />
+      </Box>
     </Box>
   )
 }
---
+++ frontend/src/components/map.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState } from 'react';
+import { APIProvider, Marker, Map } from '@vis.gl/react-google-maps';
+import { useGeolocated } from 'react-geolocated';
+
+const MapComponent = () => {
+
+  // const [selectedPlace, setSelectedPlace] = useState(null);
+
+  const { coords } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+  });
+
+  return (
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API}>
+      <Map
+        style={{ width: '100%', height: '100%' }}
+        center={coords ? { lat: coords.latitude, lng: coords.longitude } : { lat: 0, lng: 0 }}
+        defaultZoom={10}
+        gestureHandling="greedy"
+        disableDefaultUI
+      >
+        <Marker position={coords ? { lat: coords.latitude, lng: coords.longitude } : { lat: 0, lng: 0 }} />
+      </Map>
+    </APIProvider>
+  );
+};
+
+export default MapComponent;
